8d5b79d8d9298ead0598c76f84f3642dc4881a29
Alexander T Charleston <53549571+Ghost1738@users.noreply.github.com>
2024-03-10 18:30:55 -0400

    Update Assignment 3.c

--- Assignment 3.c
+++ Assignment 3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_CUSTOMERS 500000
 #define MAX_BOOTHS 12
@@ -7,9 +8,16 @@ typedef struct {
     char name[51]; // 1 to 50 uppercase letters + '\0'
     int numTickets;
     int arrivalTime;
-    int boothNumber;
     int transactionTime;
 } Customer;
+
+typedef struct {
+    Customer *data;
+    int front, rear;
+} Queue;
+
+void enqueue(Queue *queue, Customer customer);
+Customer dequeue(Queue *queue);
 
 void processCustomers(Customer customers[], int numCustomers, int numBooths);
 
@@ -37,21 +45,48 @@ int main() {
     return 0;
 }
 
+void enqueue(Queue *queue, Customer customer) {
+    queue->data[queue->rear++] = customer;
+}
+
+Customer dequeue(Queue *queue) {
+    return queue->data[queue->front++];
+}
+
 void processCustomers(Customer customers[], int numCustomers, int numBooths) {
+    Queue boothQueues[MAX_BOOTHS];
+
+    for (int i = 0; i < numBooths; ++i) {
+        boothQueues[i].data = (Customer *)malloc(sizeof(Customer) * numCustomers);
+        boothQueues[i].front = boothQueues[i].rear = 0;
+    }
+
+    // Distribute customers to booth queues based on arrival time
+    for (int i = 0; i < numCustomers; ++i) {
+        int boothIndex = i % numBooths;
+        enqueue(&boothQueues[boothIndex], customers[i]);
+    }
+
     int currentTime = 0;
 
-    for (int booth = 1; booth <= numBooths; ++booth) {
-        printf("Booth %d\n", booth);
+    for (int booth = 0; booth < numBooths; ++booth) {
+        printf("Booth %d\n", booth + 1);
 
-        for (int i = 0; i < numCustomers; ++i) {
-            if (customers[i].boothNumber == booth) {
-                // Output customer information
-                printf("%s from line %d checks out at time %d.\n",
-                       customers[i].name, i + 1, customers[i].transactionTime + currentTime);
-            }
+        while (boothQueues[booth].front < boothQueues[booth].rear) {
+            // Dequeue and process customers from the booth queue
+            Customer currentCustomer = dequeue(&boothQueues[booth]);
+            currentCustomer.transactionTime = currentTime;
+            printf("%s from line %d checks out at time %d.\n",
+                   currentCustomer.name, currentCustomer.arrivalTime, currentCustomer.transactionTime);
+            currentTime += currentCustomer.numTickets * 2; // Assuming 2 seconds per ticket
         }
 
         // Output a blank line after each booth
         printf("\n");
     }
+
+    for (int i = 0; i < numBooths; ++i) {
+        free(boothQueues[i].data);
+    }
 }
+
